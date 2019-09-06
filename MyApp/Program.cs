cbd6ce78f4204a2f56cb3af0482c88cde6e8636d
Opilane TTHK <opilane@tthk.ee>
2019-09-06 18:18:43 +0300

    kommentaar

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -12,6 +12,11 @@ namespace MyApp
             name = Console.ReadLine();
         
             Console.WriteLine(" Hello " + name);
+            Console.WriteLine("when were you born? Please enter the year: ");
+            int year;
+            year = int.Parse(Console.ReadLine());
+            int age = 2019 - year;
+            Console.WriteLine("you are " + age + " years old");
             Console.ReadLine();
         
 
